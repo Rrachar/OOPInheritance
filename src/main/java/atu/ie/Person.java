5ba2d9cdbd532fddf292975e3539de31205b352e
Rrachar <g00440528@atu.ie>
2025-12-01 09:34:20 +0000

    added getters and setters

--- src/main/java/atu/ie/Person.java
+++ src/main/java/atu/ie/Person.java
@@ -2,4 +2,22 @@ package atu.ie;
 
 
 public class Person {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    //default constructor
+    public Person(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    //getters and setters
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+    public String getAddress() {return address;}
+    public void setAddress(String address) {this.address = address;}
+    public String getPhoneNumber() {return phoneNumber;}
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
 }
