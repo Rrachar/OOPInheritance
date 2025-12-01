55f1ce397393516e55f81c1011bb53f904711c97
Rrachar <g00440528@atu.ie>
2025-12-01 10:57:56 +0000

    FINISHED IT OFF

--- src/main/java/atu/ie/PrefferedCustomer.java
+++ src/main/java/atu/ie/PrefferedCustomer.java
@@ -1,45 +1,88 @@
 package atu.ie;
 
 public class PrefferedCustomer extends Customer {
-    private int loyaltyPoints;
-    private double discountLevel; // 0.05 for 5%, etc.
 
-    //no org constructor
-    public prefferedCustomer(){
-        super();
+    private int loyaltyPoints;
+    private double discountLevel; // 0.05 = 5%
+
+
+    // No-argument constructor
+    public PrefferedCustomer() {
+        super();        // calls Customer() which calls Person()
         this.loyaltyPoints = 0;
-        this.discountLevel = 0;
+        this.discountLevel = 0.0;
     }
-    public prefferedCustomer(String name,
-                             String address,
-                             String phoneNumber,
-                             int customerNumber,
-                             boolean mailingList,
-                             int loyaltyPoints){
-        super(name,address,phoneNumber,customerNumber,mailingList);
+
+
+    // Parameterised constructor
+
+    public PrefferedCustomer(
+            String name,
+            String address,
+            String phoneNumber,
+            int customerNumber,
+            boolean mailingList,
+            int loyaltyPoints
+    ) {
+        super(name, address, phoneNumber, customerNumber, mailingList);
+
         this.loyaltyPoints = loyaltyPoints;
-        this.discountLevel = 0;
+
+        updateDiscountLevel();   // set discount based on points
     }
+
+
+    // Private helper: updates discount
 
     private void updateDiscountLevel() {
-        // set discountLevel based on loyaltyPoints
-        if(this.loyaltyPoints < 500){
-            this.discountLevel = 0.0;
-        }
-        if(this.loyaltyPoints > 500 && this.loyaltyPoints < 1000){
-            this.discountLevel = 0.05;
-        }
-        if(this.loyaltyPoints > 1000 && this.loyaltyPoints < 1500){
-            this.discountLevel = 0.06;
-        }
-        if(this.loyaltyPoints > 1500 && this.loyaltyPoints < 2000){
-            this.discountLevel = 0.07;
-        }
-        if(this.loyaltyPoints >= 2000){
-            this.discountLevel = 0.10;
+        if (loyaltyPoints < 500) {
+            discountLevel = 0.00;
+        } else if (loyaltyPoints < 1000) {
+            discountLevel = 0.05;
+        } else if (loyaltyPoints < 1500) {
+            discountLevel = 0.06;
+        } else if (loyaltyPoints < 2000) {
+            discountLevel = 0.07;
+        } else {
+            discountLevel = 0.10;
         }
     }
 
+    // Public method to add points
+    public void addLoyaltyPoints(int points) {
+        this.loyaltyPoints += points;
+        updateDiscountLevel();
+    }
+
+    // Getters and Setters
+    public int getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    public void setLoyaltyPoints(int loyaltyPoints) {
+        this.loyaltyPoints = loyaltyPoints;
+        updateDiscountLevel();   // keep discount in sync
+    }
+
+    public double getDiscountLevel() {
+        return discountLevel;
+    }
+
+    public void setDiscountLevel(double discountLevel) {
+        this.discountLevel = discountLevel;
+        // (optional: you could restrict this to update only via updateDiscountLevel())
+    }
+
+
+    // toString()
+    @Override
+    public String toString() {
+        return "PreferredCustomer{" +
+                "customer=" + super.toString() +
+                ", loyaltyPoints=" + loyaltyPoints +
+                ", discountLevel=" + discountLevel +
+                '}';
+    }
 }
 
 
@@ -52,5 +95,4 @@ public class PrefferedCustomer extends Customer {
 
 
 
-//behaviour
-public void addLoyaktyPoints(int points)
+
